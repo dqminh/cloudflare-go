9dc9adc2702e0284a7bb884dc857c9237b9481f0
Chris Branch <cbranch@cloudflare.com>
2017-07-24 22:24:38 +0100

    Implement Load Balancer Monitor APIs

--- load_balancing.go
+++ load_balancing.go
@@ -26,6 +26,23 @@ type LoadBalancerOrigin struct {
 	Enabled bool   `json:"enabled"`
 }
 
+// LoadBalancerMonitor represents a load balancer monitor's properties.
+type LoadBalancerMonitor struct {
+	ID            string              `json:"id,omitempty"`
+	CreatedOn     *time.Time          `json:"created_on,omitempty"`
+	ModifiedOn    *time.Time          `json:"modified_on,omitempty"`
+	Type          string              `json:"type"`
+	Description   string              `json:"description"`
+	Method        string              `json:"method"`
+	Path          string              `json:"path"`
+	Header        map[string][]string `json:"header"`
+	Timeout       int                 `json:"timeout"`
+	Retries       int                 `json:"retries"`
+	Interval      int                 `json:"interval"`
+	ExpectedBody  string              `json:"expected_body"`
+	ExpectedCodes string              `json:"expected_codes"`
+}
+
 // loadBalancerPoolResponse represents the response from the load balancer pool endpoints.
 type loadBalancerPoolResponse struct {
 	Response
@@ -37,6 +54,19 @@ type loadBalancerPoolListResponse struct {
 	Response
 	Result     []LoadBalancerPool `json:"result"`
 	ResultInfo ResultInfo         `json:"result_info"`
+}
+
+// loadBalancerMonitorResponse represents the response from the load balancer monitor endpoints.
+type loadBalancerMonitorResponse struct {
+	Response
+	Result LoadBalancerMonitor `json:"result"`
+}
+
+// loadBalancerMonitorListResponse represents the response from the List Monitors endpoint.
+type loadBalancerMonitorListResponse struct {
+	Response
+	Result     []LoadBalancerMonitor `json:"result"`
+	ResultInfo ResultInfo            `json:"result_info"`
 }
 
 // CreateLoadBalancerPool creates a new load balancer pool.
@@ -110,6 +140,81 @@ func (api *API) ModifyLoadBalancerPool(pool LoadBalancerPool) (LoadBalancerPool,
 	var r loadBalancerPoolResponse
 	if err := json.Unmarshal(res, &r); err != nil {
 		return LoadBalancerPool{}, errors.Wrap(err, errUnmarshalError)
+	}
+	return r.Result, nil
+}
+
+// CreateLoadBalancerMonitor creates a new load balancer monitor.
+//
+// API reference: https://api.cloudflare.com/#load-balancer-monitors-create-a-monitor
+func (api *API) CreateLoadBalancerMonitor(monitor LoadBalancerMonitor) (LoadBalancerMonitor, error) {
+	uri := "/user/load_balancers/monitors"
+	res, err := api.makeRequest("POST", uri, monitor)
+	if err != nil {
+		return LoadBalancerMonitor{}, errors.Wrap(err, errMakeRequestError)
+	}
+	var r loadBalancerMonitorResponse
+	if err := json.Unmarshal(res, &r); err != nil {
+		return LoadBalancerMonitor{}, errors.Wrap(err, errUnmarshalError)
+	}
+	return r.Result, nil
+}
+
+// ListLoadBalancerMonitors lists load balancer monitors connected to an account.
+//
+// API reference: https://api.cloudflare.com/#load-balancer-monitors-list-monitors
+func (api *API) ListLoadBalancerMonitors() ([]LoadBalancerMonitor, error) {
+	uri := "/user/load_balancers/monitors"
+	res, err := api.makeRequest("GET", uri, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, errMakeRequestError)
+	}
+	var r loadBalancerMonitorListResponse
+	if err := json.Unmarshal(res, &r); err != nil {
+		return nil, errors.Wrap(err, errUnmarshalError)
+	}
+	return r.Result, nil
+}
+
+// LoadBalancerMonitorDetails returns the details for a load balancer monitor.
+//
+// API reference: https://api.cloudflare.com/#load-balancer-monitors-monitor-details
+func (api *API) LoadBalancerMonitorDetails(monitorID string) (LoadBalancerMonitor, error) {
+	uri := "/user/load_balancers/monitors/" + monitorID
+	res, err := api.makeRequest("GET", uri, nil)
+	if err != nil {
+		return LoadBalancerMonitor{}, errors.Wrap(err, errMakeRequestError)
+	}
+	var r loadBalancerMonitorResponse
+	if err := json.Unmarshal(res, &r); err != nil {
+		return LoadBalancerMonitor{}, errors.Wrap(err, errUnmarshalError)
+	}
+	return r.Result, nil
+}
+
+// DeleteLoadBalancerMonitor disables and deletes a load balancer monitor.
+//
+// API reference: https://api.cloudflare.com/#load-balancer-monitors-delete-a-monitor
+func (api *API) DeleteLoadBalancerMonitor(monitorID string) error {
+	uri := "/user/load_balancers/monitors/" + monitorID
+	if _, err := api.makeRequest("DELETE", uri, nil); err != nil {
+		return errors.Wrap(err, errMakeRequestError)
+	}
+	return nil
+}
+
+// ModifyLoadBalancerMonitor modifies a configured load balancer monitor.
+//
+// API reference: https://api.cloudflare.com/#load-balancer-monitors-modify-a-monitor
+func (api *API) ModifyLoadBalancerMonitor(monitor LoadBalancerMonitor) (LoadBalancerMonitor, error) {
+	uri := "/user/load_balancers/monitors/" + monitor.ID
+	res, err := api.makeRequest("PUT", uri, monitor)
+	if err != nil {
+		return LoadBalancerMonitor{}, errors.Wrap(err, errMakeRequestError)
+	}
+	var r loadBalancerMonitorResponse
+	if err := json.Unmarshal(res, &r); err != nil {
+		return LoadBalancerMonitor{}, errors.Wrap(err, errUnmarshalError)
 	}
 	return r.Result, nil
 }
--- load_balancing_test.go
+++ load_balancing_test.go
@@ -338,3 +338,372 @@ func TestModifyLoadBalancerPool(t *testing.T) {
 		assert.Equal(t, want, actual)
 	}
 }
+
+func TestCreateLoadBalancerMonitor(t *testing.T) {
+	setup()
+	defer teardown()
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, r.Method, "POST", "Expected method 'POST', got %s", r.Method)
+		w.Header().Set("content-type", "application/json")
+		b, err := ioutil.ReadAll(r.Body)
+		defer r.Body.Close()
+		if assert.NoError(t, err) {
+			assert.JSONEq(t, `{
+              "type": "https",
+              "description": "Login page monitor",
+              "method": "GET",
+              "path": "/health",
+              "header": {
+                "Host": [
+                  "example.com"
+                ],
+                "X-App-ID": [
+                  "abc123"
+                ]
+              },
+              "timeout": 3,
+              "retries": 0,
+              "interval": 90,
+              "expected_body": "alive",
+              "expected_codes": "2xx"
+						}`, string(b))
+		}
+		fmt.Fprint(w, `{
+            "success": true,
+            "errors": [],
+            "messages": [],
+            "result": {
+                "id": "f1aba936b94213e5b8dca0c0dbf1f9cc",
+                "created_on": "2014-01-01T05:20:00.12345Z",
+                "modified_on": "2014-02-01T05:20:00.12345Z",
+                "type": "https",
+                "description": "Login page monitor",
+                "method": "GET",
+                "path": "/health",
+                "header": {
+                  "Host": [
+                    "example.com"
+                  ],
+                  "X-App-ID": [
+                    "abc123"
+                  ]
+                },
+                "timeout": 3,
+                "retries": 0,
+                "interval": 90,
+                "expected_body": "alive",
+                "expected_codes": "2xx"
+            }
+        }`)
+	}
+
+	mux.HandleFunc("/user/load_balancers/monitors", handler)
+	createdOn, _ := time.Parse(time.RFC3339, "2014-01-01T05:20:00.12345Z")
+	modifiedOn, _ := time.Parse(time.RFC3339, "2014-02-01T05:20:00.12345Z")
+	want := LoadBalancerMonitor{
+		ID:          "f1aba936b94213e5b8dca0c0dbf1f9cc",
+		CreatedOn:   &createdOn,
+		ModifiedOn:  &modifiedOn,
+		Type:        "https",
+		Description: "Login page monitor",
+		Method:      "GET",
+		Path:        "/health",
+		Header: map[string][]string{
+			"Host":     []string{"example.com"},
+			"X-App-ID": []string{"abc123"},
+		},
+		Timeout:       3,
+		Retries:       0,
+		Interval:      90,
+		ExpectedBody:  "alive",
+		ExpectedCodes: "2xx",
+	}
+	request := LoadBalancerMonitor{
+		Type:        "https",
+		Description: "Login page monitor",
+		Method:      "GET",
+		Path:        "/health",
+		Header: map[string][]string{
+			"Host":     []string{"example.com"},
+			"X-App-ID": []string{"abc123"},
+		},
+		Timeout:       3,
+		Retries:       0,
+		Interval:      90,
+		ExpectedBody:  "alive",
+		ExpectedCodes: "2xx",
+	}
+
+	actual, err := client.CreateLoadBalancerMonitor(request)
+	if assert.NoError(t, err) {
+		assert.Equal(t, want, actual)
+	}
+}
+
+func TestListLoadBalancerMonitors(t *testing.T) {
+	setup()
+	defer teardown()
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, r.Method, "GET", "Expected method 'GET', got %s", r.Method)
+		w.Header().Set("content-type", "application/json")
+		fmt.Fprint(w, `{
+            "success": true,
+            "errors": [],
+            "messages": [],
+            "result": [
+                {
+                    "id": "f1aba936b94213e5b8dca0c0dbf1f9cc",
+                    "created_on": "2014-01-01T05:20:00.12345Z",
+                    "modified_on": "2014-02-01T05:20:00.12345Z",
+                    "type": "https",
+                    "description": "Login page monitor",
+                    "method": "GET",
+                    "path": "/health",
+                    "header": {
+                      "Host": [
+                        "example.com"
+                      ],
+                      "X-App-ID": [
+                        "abc123"
+                      ]
+                    },
+                    "timeout": 3,
+                    "retries": 0,
+                    "interval": 90,
+                    "expected_body": "alive",
+                    "expected_codes": "2xx"
+                }
+            ],
+            "result_info": {
+                "page": 1,
+                "per_page": 20,
+                "count": 1,
+                "total_count": 2000
+            }
+        }`)
+	}
+
+	mux.HandleFunc("/user/load_balancers/monitors", handler)
+	createdOn, _ := time.Parse(time.RFC3339, "2014-01-01T05:20:00.12345Z")
+	modifiedOn, _ := time.Parse(time.RFC3339, "2014-02-01T05:20:00.12345Z")
+	want := []LoadBalancerMonitor{
+		{
+			ID:          "f1aba936b94213e5b8dca0c0dbf1f9cc",
+			CreatedOn:   &createdOn,
+			ModifiedOn:  &modifiedOn,
+			Type:        "https",
+			Description: "Login page monitor",
+			Method:      "GET",
+			Path:        "/health",
+			Header: map[string][]string{
+				"Host":     []string{"example.com"},
+				"X-App-ID": []string{"abc123"},
+			},
+			Timeout:       3,
+			Retries:       0,
+			Interval:      90,
+			ExpectedBody:  "alive",
+			ExpectedCodes: "2xx",
+		},
+	}
+
+	actual, err := client.ListLoadBalancerMonitors()
+	if assert.NoError(t, err) {
+		assert.Equal(t, want, actual)
+	}
+}
+
+func TestLoadBalancerMonitorDetails(t *testing.T) {
+	setup()
+	defer teardown()
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, r.Method, "GET", "Expected method 'GET', got %s", r.Method)
+		w.Header().Set("content-type", "application/json")
+		fmt.Fprint(w, `{
+            "success": true,
+            "errors": [],
+            "messages": [],
+            "result": {
+                "id": "f1aba936b94213e5b8dca0c0dbf1f9cc",
+                "created_on": "2014-01-01T05:20:00.12345Z",
+                "modified_on": "2014-02-01T05:20:00.12345Z",
+                "type": "https",
+                "description": "Login page monitor",
+                "method": "GET",
+                "path": "/health",
+                "header": {
+                  "Host": [
+                    "example.com"
+                  ],
+                  "X-App-ID": [
+                    "abc123"
+                  ]
+                },
+                "timeout": 3,
+                "retries": 0,
+                "interval": 90,
+                "expected_body": "alive",
+                "expected_codes": "2xx"
+            }
+        }`)
+	}
+
+	mux.HandleFunc("/user/load_balancers/monitors/f1aba936b94213e5b8dca0c0dbf1f9cc", handler)
+	createdOn, _ := time.Parse(time.RFC3339, "2014-01-01T05:20:00.12345Z")
+	modifiedOn, _ := time.Parse(time.RFC3339, "2014-02-01T05:20:00.12345Z")
+	want := LoadBalancerMonitor{
+		ID:          "f1aba936b94213e5b8dca0c0dbf1f9cc",
+		CreatedOn:   &createdOn,
+		ModifiedOn:  &modifiedOn,
+		Type:        "https",
+		Description: "Login page monitor",
+		Method:      "GET",
+		Path:        "/health",
+		Header: map[string][]string{
+			"Host":     []string{"example.com"},
+			"X-App-ID": []string{"abc123"},
+		},
+		Timeout:       3,
+		Retries:       0,
+		Interval:      90,
+		ExpectedBody:  "alive",
+		ExpectedCodes: "2xx",
+	}
+
+	actual, err := client.LoadBalancerMonitorDetails("f1aba936b94213e5b8dca0c0dbf1f9cc")
+	if assert.NoError(t, err) {
+		assert.Equal(t, want, actual)
+	}
+
+	_, err = client.LoadBalancerMonitorDetails("bar")
+	assert.Error(t, err)
+}
+
+func TestDeleteLoadBalancerMonitor(t *testing.T) {
+	setup()
+	defer teardown()
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, r.Method, "DELETE", "Expected method 'DELETE', got %s", r.Method)
+		w.Header().Set("content-type", "application/json")
+		fmt.Fprint(w, `{
+            "success": true,
+            "errors": [],
+            "messages": [],
+            "result": {
+              "id": "f1aba936b94213e5b8dca0c0dbf1f9cc"
+            }
+        }`)
+	}
+
+	mux.HandleFunc("/user/load_balancers/monitors/f1aba936b94213e5b8dca0c0dbf1f9cc", handler)
+	assert.NoError(t, client.DeleteLoadBalancerMonitor("f1aba936b94213e5b8dca0c0dbf1f9cc"))
+	assert.Error(t, client.DeleteLoadBalancerMonitor("bar"))
+}
+
+func TestModifyLoadBalancerMonitor(t *testing.T) {
+	setup()
+	defer teardown()
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, r.Method, "PUT", "Expected method 'PUT', got %s", r.Method)
+		w.Header().Set("content-type", "application/json")
+		b, err := ioutil.ReadAll(r.Body)
+		defer r.Body.Close()
+		if assert.NoError(t, err) {
+			assert.JSONEq(t, `{
+                "id": "f1aba936b94213e5b8dca0c0dbf1f9cc",
+                "type": "http",
+                "description": "Login page monitor",
+                "method": "GET",
+                "path": "/status",
+                "header": {
+                  "Host": [
+                    "example.com"
+                  ],
+                  "X-App-ID": [
+                    "easy"
+                  ]
+                },
+                "timeout": 3,
+                "retries": 0,
+                "interval": 90,
+                "expected_body": "kicking",
+                "expected_codes": "200"
+						}`, string(b))
+		}
+		fmt.Fprint(w, `{
+            "success": true,
+            "errors": [],
+            "messages": [],
+            "result": {
+                "id": "f1aba936b94213e5b8dca0c0dbf1f9cc",
+                "created_on": "2014-01-01T05:20:00.12345Z",
+                "modified_on": "2017-02-01T05:20:00.12345Z",
+                "type": "http",
+                "description": "Login page monitor",
+                "method": "GET",
+                "path": "/status",
+                "header": {
+                  "Host": [
+                    "example.com"
+                  ],
+                  "X-App-ID": [
+                    "easy"
+                  ]
+                },
+                "timeout": 3,
+                "retries": 0,
+                "interval": 90,
+                "expected_body": "kicking",
+                "expected_codes": "200"
+            }
+        }`)
+	}
+
+	mux.HandleFunc("/user/load_balancers/monitors/f1aba936b94213e5b8dca0c0dbf1f9cc", handler)
+	createdOn, _ := time.Parse(time.RFC3339, "2014-01-01T05:20:00.12345Z")
+	modifiedOn, _ := time.Parse(time.RFC3339, "2017-02-01T05:20:00.12345Z")
+	want := LoadBalancerMonitor{
+		ID:          "f1aba936b94213e5b8dca0c0dbf1f9cc",
+		CreatedOn:   &createdOn,
+		ModifiedOn:  &modifiedOn,
+		Type:        "http",
+		Description: "Login page monitor",
+		Method:      "GET",
+		Path:        "/status",
+		Header: map[string][]string{
+			"Host":     []string{"example.com"},
+			"X-App-ID": []string{"easy"},
+		},
+		Timeout:       3,
+		Retries:       0,
+		Interval:      90,
+		ExpectedBody:  "kicking",
+		ExpectedCodes: "200",
+	}
+	request := LoadBalancerMonitor{
+		ID:          "f1aba936b94213e5b8dca0c0dbf1f9cc",
+		Type:        "http",
+		Description: "Login page monitor",
+		Method:      "GET",
+		Path:        "/status",
+		Header: map[string][]string{
+			"Host":     []string{"example.com"},
+			"X-App-ID": []string{"easy"},
+		},
+		Timeout:       3,
+		Retries:       0,
+		Interval:      90,
+		ExpectedBody:  "kicking",
+		ExpectedCodes: "200",
+	}
+
+	actual, err := client.ModifyLoadBalancerMonitor(request)
+	if assert.NoError(t, err) {
+		assert.Equal(t, want, actual)
+	}
+}
